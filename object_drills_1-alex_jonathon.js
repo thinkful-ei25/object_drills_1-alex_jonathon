300f6a236c82b638c903c7552afc8c3c91f399a3
talexcrowell <talexcrowell@gmail.com>
2018-09-26 09:26:49 -0700

    added self_reference.js

--- object_drills_1-alex_jonathon.js
+++ object_drills_1-alex_jonathon.js
@@ -7,6 +7,7 @@ function createMyObject() {
       console.log('hello');
     } 
   }
+
   return obj;
 }
 
---
+++ self_reference_drill.js
@@ -0,0 +1,12 @@
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+  return person;
+}
+let obj = personMaker();
+console.log(obj.fullName());
