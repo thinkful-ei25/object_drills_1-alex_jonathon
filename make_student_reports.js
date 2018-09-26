8629aac2b3636a474f2ff91de067b2020b9bb854
talexcrowell <talexcrowell@gmail.com>
2018-09-26 09:37:48 -0700

    added make_student_reports.js

---
+++ make_student_reports.js
@@ -0,0 +1,15 @@
+const data = [
+    { name: 'Jane Doe', grade: 'A' },
+    { name: 'John Dough', grade: 'B' },
+    { name: 'Jill Do', grade: 'A' },
+  ];
+
+function makeStudentReport (data) {
+  let arr = [];
+  for (let i =0; i < data.length; i++) {
+    arr.push(data[i].name + " : " + data[i].grade); 
+  }
+  return arr;
+}
+
+console.log(makeStudentReport(data));
