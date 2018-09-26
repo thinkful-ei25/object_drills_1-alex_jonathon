79f04c619a5bea2f9741dd759bf7af0c002271d2
Reldalk <Reldalk@gmail.com>
2018-09-26 12:29:55 -0400

    deleting keys assignment

---
+++ deleting_keys.js
@@ -0,0 +1,14 @@
+const sampleObj = {
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+  };
+  
+  function keyDeleter(obj) {
+    delete sampleObj.foo;
+    delete sampleObj.bar;
+    return obj;
+  }
+
+  console.log(keyDeleter(sampleObj));
