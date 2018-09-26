0cfe004668390cf2fc56939d6f82db17e29754bb
talexcrowell <talexcrowell@gmail.com>
2018-09-26 11:07:27 -0700

    added object_initialize_and_methods.js

--- make_student_reports.js
+++ make_student_reports.js
@@ -1,13 +1,15 @@
+'use strict';
+
 const data = [
-    { name: 'Jane Doe', grade: 'A' },
-    { name: 'John Dough', grade: 'B' },
-    { name: 'Jill Do', grade: 'A' },
-  ];
+  { name: 'Jane Doe', grade: 'A' },
+  { name: 'John Dough', grade: 'B' },
+  { name: 'Jill Do', grade: 'A' },
+];
 
 function makeStudentReport (data) {
   let arr = [];
   for (let i =0; i < data.length; i++) {
-    arr.push(data[i].name + " : " + data[i].grade); 
+    arr.push(data[i].name + ' : ' + data[i].grade); 
   }
   return arr;
 }
--- self_reference_drill.js
+++ self_reference_drill.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function personMaker() {
   var person = {
     firstName: 'Paul',
