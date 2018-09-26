8bae6bdbd2f0fa46fa2261f0384d9fd92f890bfe
talexcrowell <talexcrowell@gmail.com>
2018-09-26 08:57:35 -0700

    added function creatMyObject

---
+++ object_drills_1-alex_jonathon.js
@@ -0,0 +1,14 @@
+function createMyObject() {
+  const obj = {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function () {
+      console.log('hello');
+    } 
+  }
+  return obj;
+}
+
+let test = createMyObject();
+console.log(test);
